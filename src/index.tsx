67090d150b9aca0f1ee6cb7240ad34e0eaff20d0
Admin <illya.shevchenko007@gmail.com>
2022-08-10 18:38:36 +0300

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+const h1 = <h1>SJKHDgh dbbbbbf gdfg</h1>
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render("dfgdgdfg");
+root.render(h1);
